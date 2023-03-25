5194477f9bb4a8c53083b2a6f56ec5f424c35e1b
dunkan_high <oleg.savinov.2002@bk.ru>
2023-03-25 19:13:02 +0300

    fix link

--- test_items.py
+++ test_items.py
@@ -1,8 +1,11 @@
 from selenium.webdriver.common.by import By
+import time
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
 def test_button_add_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
     browser.get(link)
+    time.sleep(3)
     button = browser.find_elements(By.CLASS_NAME, "btn-add-to-basket")
     assert len(button) != 0, "Button didn't find"
